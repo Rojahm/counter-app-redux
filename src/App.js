cb37154c09a17653363ed0c9cd830bd4b8b9513d
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-07 12:24:34 +0330

    import useSelector and useDispatch and actions and implement them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./reducers/counterSlice";
+//or import {actions} from ./reducers/...
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5 text-center">
+      <h2 className="mb-3">React Js Redux Store with Slice State Examle</h2>
+      <p className="h1">{count}</p>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+        className="me-2 btn btn-primary"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+        className="me-2 btn btn-danger"
+      >
+        Decrement
+      </button>
     </div>
   );
 }
-
 export default App;
